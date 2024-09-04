b4da5a763b8004674d372ac4ac7419bfcc1a22d4
sarka99 <119513443+sarka99@users.noreply.github.com>
2024-09-04 09:43:09 +0200

    Springboot backend of the calculator , first push

---
+++ src/main/java/com/sarka99/RPMCalculator/RpmCalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.sarka99.RPMCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpmCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpmCalculatorApplication.class, args);
+		System.out.println("main program run");
+	}
+
+}
---
+++ src/test/java/com/sarka99/RPMCalculator/RpmCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarka99.RPMCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpmCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
