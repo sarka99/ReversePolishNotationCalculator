397572e2e6ba66a272b0c7a915afd76f2f9b92c1
sarka99 <119513443+sarka99@users.noreply.github.com>
2024-09-04 12:18:50 +0200

    *Backend finished, but improvements could be made. *Array of strings passed in as rpnExpression, it is then evaluated by the business side (CalculatorService)

--- src/main/java/com/sarka99/RPMCalculator/controller/CalculatorController.java
+++ src/main/java/com/sarka99/RPMCalculator/controller/CalculatorController.java
@@ -1,4 +1,19 @@
 package com.sarka99.RPMCalculator.controller;
 
+import com.sarka99.RPMCalculator.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+    @Autowired
+    private CalculatorService calculatorService;
+    @PostMapping("/evaluate")
+    public int EvaluateExpression(@RequestBody String[] rpnExpression){
+
+        return calculatorService.EvaluateRPN(rpnExpression);
+    }
 }
--- src/main/java/com/sarka99/RPMCalculator/service/CalculatorService.java
+++ src/main/java/com/sarka99/RPMCalculator/service/CalculatorService.java
@@ -1,4 +1,35 @@
 package com.sarka99.RPMCalculator.service;
 
+import com.sarka99.RPMCalculator.model.Stack;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+    Stack<Integer> stack = new Stack<>();
+    public int EvaluateRPN(String [] rpnExpression){
+        //Going through each token, and pushing/popping to the stack depending on the current token
+        for(String token : rpnExpression){
+            switch (token){
+                case "+":
+                stack.push(stack.pop() + stack.pop());
+                break;
+                case "-":
+                    int subtractor = stack.pop();
+                    stack.push(stack.pop() - subtractor);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int divisor = stack.pop();
+                    stack.push(stack.pop() / divisor);
+                    break;
+                default:
+                    //If an operand is the current token, it's simply added to the stack
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+        //Returns the final answer, which is the final value in the stack
+        return stack.pop();
+    }
 }
