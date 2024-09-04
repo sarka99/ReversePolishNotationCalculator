708c9c37d013fe3eb5e974bb712c072b4bc80211
sarka99 <119513443+sarka99@users.noreply.github.com>
2024-09-04 13:58:47 +0200

    Enabled cors for use in the frontend + printed out rpn expression sent from the frontend

--- src/main/java/com/sarka99/RPMCalculator/controller/CalculatorController.java
+++ src/main/java/com/sarka99/RPMCalculator/controller/CalculatorController.java
@@ -13,7 +13,13 @@ public class CalculatorController {
     private CalculatorService calculatorService;
     @PostMapping("/evaluate")
     public int EvaluateExpression(@RequestBody String[] rpnExpression){
-
+        StringBuilder sb = new StringBuilder();
+        for(String s : rpnExpression){
+            sb.append(s);
+        }
+        System.out.println("rpn expression from the frontend: " + sb.toString());
+        System.out.println("Evaluating expression from backend called");
+        System.out.println("result of evaluation is:" + calculatorService.EvaluateRPN(rpnExpression));
         return calculatorService.EvaluateRPN(rpnExpression);
     }
 }
